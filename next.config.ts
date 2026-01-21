c164db74219e33d182e83ce1db1f36b9f1936ee1
Mihaly Gyori <kenyerafejeden@gmail.com>
2026-01-21 12:39:48 +0100

    Added images and new workplace

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ import type { NextConfig } from 'next';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-    output: 'export',
+    // output: 'export',
     trailingSlash: true,
     images: {
         unoptimized: true,
